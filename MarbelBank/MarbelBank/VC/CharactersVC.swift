e7782fd6e1fb20d264ec5d6a2dec42ecf2c0b167
algonrey <wuhumail@gmail.com>
2022-01-22 15:51:10 +0100

    Project creation / reorganization, add app icon

---
+++ MarbelBank/MarbelBank/VC/CharactersVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarbelBank
+//
+//  Created by Berto on 22/1/22.
+//
+
+import UIKit
+
+class CharactersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
